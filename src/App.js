f6e17ef619932d622127c8ec7776040620c5d8a2
ilane madar <madarilane@hotmail.com>
2020-02-24 11:32:30 +0200

    div

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
-import React, { Component } from 'react'
-import Card from "./components/Cards"
+import React, { Component } from "react";
+import Card from "./components/card/Card";
+import { robots } from "./robots";
+import "tachyons";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      <Card />
-      <Card />
-      <Card />
-      
+      <div>
+        <Card
+          id={robots[0].id}
+          name={robots[0].name}
+          email={robots[0].email}
+          usrn={robots[0].username}
+        />
+        <Card
+          id={robots[1].id}
+          name={robots[1].name}
+          email={robots[1].email}
+          usrn={robots[1].username}
+        />
+        <Card
+          id={robots[2].id}
+          name={robots[2].name}
+          email={robots[2].email}
+          usrn={robots[2].username}
+        />
+        <Card
+          id={robots[3].id}
+          name={robots[3].name}
+          email={robots[3].email}
+          usrn={robots[3].username}
+        />
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
---
+++ src/components/card/Card.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div className="bg-light-green dib br3 ma2 grow bw2 shadow-5">
+        <img alt="robot" src="https://robohash.org/Ilane?size=200x200"/>
+      <div>
+        <h2>C3PO</h2>
+        <p>C3PO@gmail.com</p>
+      </div>
+    </div>
+  );
+};
+export default Card;
