c0325a3b2116c24ada35bdcb32371f8aebc3badc
ilane madar <madarilane@hotmail.com>
2020-02-25 12:10:13 +0200

    Updates

---
+++ precache-manifest.e7d6cde4d39ea2e97c60a74d75fbe613.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0104cc4de5d3f099be9d784476901bc4",
+    "url": "/robotapp/index.html"
+  },
+  {
+    "revision": "62f4307641e9617cda47",
+    "url": "/robotapp/static/css/2.299a5230.chunk.css"
+  },
+  {
+    "revision": "97766a9d5f840f096d8d",
+    "url": "/robotapp/static/css/main.63895d8a.chunk.css"
+  },
+  {
+    "revision": "62f4307641e9617cda47",
+    "url": "/robotapp/static/js/2.ff5b2bd8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/robotapp/static/js/2.ff5b2bd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97766a9d5f840f096d8d",
+    "url": "/robotapp/static/js/main.6b42a8bc.chunk.js"
+  },
+  {
+    "revision": "9057cbaf814dabc4834d",
+    "url": "/robotapp/static/js/runtime-main.59da4049.js"
+  }
+]);
